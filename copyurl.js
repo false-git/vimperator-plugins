7c5f7378164f8d2df7d381b58b92ee2cd4e15c95
false <false@wizard-limit.net>
2011-05-08 17:03:49 +0900

    add

---
+++ copyurl.js
@@ -0,0 +1,28 @@
+// PLUGIN_INFO//{{{
+var PLUGIN_INFO =
+<VimperatorPlugin>
+    <name>copyurl</name>
+    <description>Copy link HTML for current page.</description>
+    <author mail="false@wizard-limit.net" homepage="http://www.wizard-limit.net/mt/pc">PC日記</author>
+    <version>0.1</version>
+    <minVersion>2.0pre</minVersion>
+    <maxVersion>3.1</maxVersion>
+    <updateURL>https://github.com/false-git/vimperator-plugins/raw/master/copyurl.js</updateURL>
+    <detail><![CDATA[
+
+== COMMANDS ==
+copyurl [URL]:
+    echo and copy URL
+
+    ]]></detail>
+</VimperatorPlugin>;
+//}}}
+
+(function() {
+    commands.add(['copyurl'], 'echo and copy link html for current url',
+        function(args) util.copyToClipboard('<a href="' + buffer.URL + '">' + buffer.title + '</a>', true),
+        {
+            argCount: '?',
+        });
+})();
+// vim: fdm=marker sw=4 ts=4 et:
